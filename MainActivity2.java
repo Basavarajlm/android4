c46b3e330b50caee7a6ac54ff8d7fe7cee54b024
Basavarajlm <96778085+Basavarajlm@users.noreply.github.com>
2022-08-03 02:13:30 +0530

    Create MainActivity2.java

---
+++ MainActivity2.java
@@ -0,0 +1,54 @@
+package com.example.pr4;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class MainActivity2 extends AppCompatActivity
+{
+    private static final int REQUEST_CODE_SELECT_IMAGE = 2;
+
+    ImageView imageSelected;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageSelected = findViewById(R.id.selectedImage);
+        findViewById(R.id.buttonSelectedImage).setOnClickListener(view -> {
+            selectImage();
+        });
+    }
+
+    private void selectImage()
+    {
+        Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,REQUEST_CODE_SELECT_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data != null){
+            Uri selectedImageUri = data.getData();
+            InputStream inputStream = null;
+            try {
+                inputStream = getContentResolver().openInputStream(selectedImageUri);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            imageSelected.setImageBitmap(bitmap);
+        }
+    }
+}
